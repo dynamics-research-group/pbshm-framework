04c090fa84789cfad1a45bc0b0fb26300e828142
Dan Brennan <d.s.brennan@sheffield.ac.uk>
2024-02-26 19:02:04 +0000

    Register sideloaded modules into system
    Calculate the site package location for each of the installed
    sideloaded modules, import the module via the module_spec,
    and register the imported module in the sytem modules
    for the desired namespace. Lastly register the module blueprint
    as a nested blueprint of the sideloader blueprint and add any
    navigation entries into the app's config.

--- rosehips/sideloader/sideloader.py
+++ rosehips/sideloader/sideloader.py
@@ -1,6 +1,9 @@
 import glob
+import importlib
+import importlib.util
 import os
 import json
+import site
 import sys
 import subprocess
 
@@ -16,6 +19,50 @@ bp = Blueprint(
     static_folder = "static",
     template_folder = "templates"
 )
+
+def register_sideloaded_modules() -> None:
+    #Calculate site packages
+    site_package_paths = site.getsitepackages()
+    default_package_path = os.path.join(site_package_paths[1 if len(site_package_paths) > 1 and sys.platform == "win32" else 0])
+    #Load modules
+    for search_file in glob.glob(os.path.join(current_app.instance_path, "*[!-disabled].module")):
+        with open(os.path.join(current_app.instance_path, search_file), "r") as module_file:
+            #Ensure path
+            module_config = json.load(module_file)
+            module_path = module_config["path"]
+            if len(module_path) < 2:
+                continue
+            #Determine path location
+            selected_module_path, default_search_path = None, os.path.join(default_package_path, *module_path[:-1])
+            if os.path.isdir(default_search_path): selected_module_path = default_search_path
+            else:
+                for potential_path in site_package_paths:
+                    current_search_path = os.path.join(potential_path, *module_path[:-1])
+                    if current_search_path == default_search_path:
+                        continue
+                    elif os.path.isdir(current_search_path):
+                        selected_module_path = current_search_path
+                        break
+            if selected_module_path is None:
+                print(f"Unable to locate path for namespace {module_config['namespace']}")
+                continue
+            #Load module
+            print(f"Registering {module_config['namespace']} from {os.path.join(selected_module_path, module_config['path'][-1])}")
+            module_spec = importlib.util.spec_from_file_location(module_config["namespace"], os.path.join(selected_module_path, module_config["path"][-1]))
+            module = importlib.util.module_from_spec(module_spec)
+            sys.modules[module_config["namespace"]] = module
+            module_spec.loader.exec_module(module)
+            #Register blueprint
+            if module_config["blueprint"]:
+                bp.register_blueprint(module.bp, url_prefix=module_config["url_prefix"])
+            #Navigation
+            for navigation_section in module_config["navigation"]:
+                if navigation_section not in current_app.config["NAVIGATION"]:
+                    current_app.config["NAVIGATION"][navigation_section] = {display_value:f"sideloader.{endpoint_value}" for (display_value, endpoint_value) in module_config["navigation"][navigation_section].items()}
+                else:
+                    for display_value in module_config["navigation"][navigation_section]:
+                        if display_value not in current_app.config["NAVIGATION"][navigation_section]:
+                            current_app.config["NAVIGATION"][navigation_section][display_value] = f"sideloader.{module_config['navigation'][navigation_section][display_value]}"
 
 @bp.route("/", methods=("GET", "POST"))
 @authenticate_request("sideloader-list")
@@ -67,7 +114,7 @@ def list_modules():
                             if len(package) == 0:
                                 error = "the name of the package cannot be empty"
                             elif package.find("git+") == 0:
-                                error = "when using a git+ package url, the name must be declared in the JSON file which matches the name given within the project build file" 
+                                error = "when using a git+ package url, the name must be declared in the JSON file which matches the name given within the project build file"
                             elif os.path.isfile(os.path.join(current_app.instance_path, f"{secure_filename(package)}-disabled.module")):
                                 error = f"there is already a pending module file for {package}"
                             elif os.path.isfile(os.path.join(current_app.instance_path, f"{secure_filename(package)}.module")):
@@ -119,4 +166,3 @@ def uninstall_module(module_name: str):
         print(f"Marking module {module_name} as disabled")
         os.rename(os.path.join(current_app.instance_path, f"{module_name}.module"), os.path.join(current_app.instance_path, f"{module_name}-disabled.module"))
     return jsonify({"status":"uninstalled"})
-    
