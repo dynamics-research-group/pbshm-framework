22b27f6651630e5f298516eb7434d2afde932024
Dan Brennan <d.s.brennan@sheffield.ac.uk>
2025-07-09 13:57:44 +0100

    Call WSGI logic from seperate script
    Remove the gunicorn code from the entrypoint and
    instead call the logic from a seperate wsgi script.

--- containers/entrypoint-framework.sh
+++ containers/entrypoint-framework.sh
@@ -153,15 +153,7 @@ setup_framework(){
 
         # Start production server
         print_pending_task_message "Starting production server"
-        exec gunicorn --workers "${GUNICORN_WORKERS:-4}" \
-            --bind "${FRONTEND_BIND:-0.0.0.0}:${FRONTEND_PORT:-5000}" \
-            --timeout "${GUNICORN_TIMEOUT:-30}" \
-            --worker-class "${GUNICORN_WORKER_CLASS:-sync}" \
-            --max-requests "${GUNICORN_MAX_REQUESTS:-0}" \
-            --max-requests-jitter "${GUNICORN_MAX_REQUESTS_JITTER:-0}" \
-            --access-logfile - \
-            --error-logfile - \
-            'rosehips:create_app()'
+        exec "$HOME/entrypoint-wsgi.sh"
         print_success_status "OK"
 
     fi
